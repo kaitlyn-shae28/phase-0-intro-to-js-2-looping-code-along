0ae6071c091fea54416961a3c4fe0242a33fc02d
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-08-14 11:11:26 -0400

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const cards = []
+const myArray = ["Sam","Pete","Emily"]
+
+function writeCards(friends, event) {
+    for (let i = 0; i < friends.length; i++){
+        cards.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`)
+        // console.log(`Thank you, ${friends[i]} for the wonderful birthday gift!`);
+        // debugger;
+    }
+    return cards
+}
+
+function countDown(){
+    for (let count = 10; count >= 0; count--)
+   console.log(count)
+}
+
